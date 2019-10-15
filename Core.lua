132443df289b8a5c27f4f1611a378c316a558d52
myrroddin <myrroddin@a59a231c-ed30-472f-94b8-dd837922ea07>
2019-10-15 14:41:54 +0000

    - First commit of files

---
+++ Core.lua
@@ -0,0 +1,208 @@
+local RepByZone = LibStub("AceAddon-3.0"):NewAddon("RepByZone", "AceEvent-3.0", "LibAboutPanel-2.0", "AceConsole-3.0")
+local L = LibStub("AceLocale-3.0"):GetLocale("RepByZone")
+local Dialog = LibStub("AceConfigDialog-3.0")
+
+-- Local variables
+local db
+local defaults = {
+    char = {
+        enabled = true,
+        watchSubZones = true,
+        verbose = true,
+    }
+}
+local zonesAndFactions = zonesAndFactions or {}
+local instancesAndFactions = instancesAndFactions or {}
+local subZonesAndFactions = subZonesAndFactions or {}
+local inCombat = false
+
+-- Get the character's racial factionID and factionName
+local function GetRacialRep()
+    local _, playerRace = UnitRace("player")
+    local racialRepID = playerRace == "Dwarf" and 47
+    or playerRace == "Gnome" and 54
+    or playerRace == "Human" and 72
+    or playerRace == "NightElf" and 69
+    or playerRace == "Orc" and 76
+    or playerRace == "Tauren" and 81
+    or playerRace == "Troll" and 530
+    or playerRace == "Scourge" and 68
+
+    local racialRepName = GetFactionInfoByID(racialRepID)
+    return racialRepID, racialRepName
+end
+
+function RepByZone:OnInitialize()
+    self.db = LibStub("AceDB-3.0"):New("RepByZoneDB", defaults, true)
+    self.db.RegisterCallback(self, "OnProfileChanged", "RefreshConfig")
+    self.db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
+    self.db.RegisterCallback(self, "OnProfileReset", "RefreshConfig")
+    self:SetEnabledState(self.db.char.enabled)
+    db = self.db.char
+
+    zonesAndFactions = self:ZoneAndFactionList() -- Data.lua
+    instancesAndFactions = self:InstancesAndFactionList() -- Data.lua
+    subZonesAndFactions = self:SubZonesAndFactions() -- Data.lua
+
+    local options = self:GetOptions() -- Options.lua
+    options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+
+    -- Support for LibAboutPanel-2.0
+    options.args.aboutTab = self:AboutOptionsTable("RepByZone")
+    options.args.aboutTab.order = -1 -- -1 means "put it last"
+
+    -- Register your options with AceConfigRegistry
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("RepByZone", options)
+
+    -- Add options to Interface/AddOns
+    self.optionsFrame = Dialog:AddToBlizOptions("RepByZone", "RepByZone")
+
+    -- Create slash commands
+    self:RegisterChatCommand("repbyzone", "SlashHandler")
+    self:RegisterChatCommand("rbz", "SlashHandler")
+
+    -- Populate db.defaultRep
+    local racialRepID, racialRepName = GetRacialRep()
+    db.defaultRepID = db.defaultRepID or racialRepID
+    db.defaultRepName = db.defaultRepName or racialRepName
+
+   -- Set initial watched faction correctly during login
+    self:SwitchedZones()
+end
+
+function RepByZone:OnEnable()
+    -- Set initial watched faction correctly during login
+    self:SwitchedZones()
+    self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "SwitchedZones")
+    if db.watchSubZones then
+        self:RegisterEvent("ZONE_CHANGED", "SwitchedSubZones")
+        self:RegisterEvent("ZONE_CHANGED_INDOORS", "SwitchedSubZones")
+    end
+end
+
+function RepByZone:OnDisable()
+    self:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
+    self:UnregisterEvent("ZONE_CHANGED")
+    self:UnregisterEvent("ZONE_CHANGED_INDOORS")
+end
+
+function RepByZone:RefreshConfig()
+    db = self.db.char
+end
+
+function RepByZone:SlashHandler()
+    if UnitAffectingCombat("player") then
+        if Dialog.OpenFrames["RepByZone"] then
+            Dialog:Close("RepByZone")
+        end
+        return
+    end
+
+    if Dialog.OpenFrames["RepByZone"] then
+        Dialog:Close("RepByZone")
+    else
+        Dialog:Open("RepByZone")
+    end
+end
+
+local repsCollapsed = {} -- Obey user's settings about headers opened or closed
+-- Open all faction headers
+function RepByZone:OpenAllFactionHeaders()
+    local i = 1
+	while i <= GetNumFactions() do
+		local name, _, _, _, _, _, _, _, isHeader, isCollapsed = GetFactionInfo(i)
+		if isHeader then
+			repsCollapsed[name] = isCollapsed
+			if name == FACTION_INACTIVE then
+				if not isCollapsed then
+					CollapseFactionHeader(i)
+				end
+				break
+			elseif isCollapsed then
+				ExpandFactionHeader(i)
+			end
+		end
+		i = i + 1
+	end
+end
+
+-- Close all faction headers
+function RepByZone:CloseAllFactionHeaders()
+    local i = 1
+	while i <= GetNumFactions() do
+		local name, _, _, _, _, _, _, _, isHeader, isCollapsed = GetFactionInfo(i)
+		if isHeader then
+			if isCollapsed and not repsCollapsed[name] then
+				ExpandFactionHeader(i)
+			elseif repsCollapsed[name] and not isCollapsed then
+				CollapseFactionHeader(i)
+			end
+		end
+		i = i + 1
+	end
+	wipe(repsCollapsed)
+end
+
+function RepByZone:GetAllFactions()
+    -- Will not return factions the user has marked as inactive
+    self:OpenAllFactionHeaders()
+    local factionList = {}
+
+    for i = 1, GetNumFactions() do
+        local name, _, _, _, _, _, _, _, isHeader, _, _, _, _, factionID = GetFactionInfo(i)
+        if not isHeader then
+            factionList[factionID] = name
+        end
+    end
+
+    self:CloseAllFactionHeaders()
+    return factionList
+end
+
+-- Blizzard sets watched faction by index, not by factionID so create our own API
+function RepByZone:SetWatchedFactionByFactionID(id)
+    if type(id) ~= "number" then return end
+
+    self:OpenAllFactionHeaders()
+    for i = 1, GetNumFactions() do
+        local name, _, standingID, _, _, _, _, _, isHeader, _, _, isWatched, _, factionID = GetFactionInfo(i)
+        -- self:Print("DEBUG: SetWatchedFactionByFactionID name:", name)
+        -- self:Print("DEBUG: SetWatchedFactionByFactionID index:", i)
+        if id == factionID then
+            if not isWatched then
+                SetWatchedFactionIndex(i)
+                if db.verbose then
+                    self:Print(L["Now watching %s"]:format(name))
+                end
+            end
+            self:CloseAllFactionHeaders()
+        end
+    end
+    self:CloseAllFactionHeaders()
+end
+
+-- Player switched zones, set watched faction
+function RepByZone:SwitchedZones()
+    local UImapID = IsInInstance() and select(8, GetInstanceInfo()) or C_Map.GetBestMapForUnit("player")
+    local locationsAndFactions = IsInInstance() and self:InstanceAndFactionList() or self:ZoneAndFactionList()
+    -- self:Print("DEBUG: Current UImapID is", UImapID)
+    for zoneID, factionID in pairs(locationsAndFactions) do
+        if zoneID == UImapID then
+            -- self:Print("DEBUG: zoneID and UImapID match")
+            self:SetWatchedFactionByFactionID(factionID)
+            break
+        end
+    end
+end
+
+-- Player entered a subzone, check if it has a faction
+function RepByZone:SwitchedSubZones()
+    if not db.watchSubZones then return end
+    self:SwitchedZones()
+    local subZone = GetSubZoneText()
+    for babbleSubZone, factionID in pairs(subZonesAndFactions) do
+        if babbleSubZone == subZone then
+            self:SetWatchedFactionByFactionID(factionID)
+        end
+    end
+end
---
+++ Data.lua
@@ -0,0 +1,161 @@
+local RepByZone = LibStub("AceAddon-3.0"):GetAddon("RepByZone")
+
+local H = UnitFactionGroup("player") == "Horde"
+local A = UnitFactionGroup("player") == "Alliance"
+local BS = LibStub and LibStub("LibBabble-SubZone-3.0", true) and LibStub("LibBabble-SubZone-3.0"):GetUnstrictLookupTable()
+
+function RepByZone:ZoneAndFactionList()
+    -- UImapID = factionID
+    -- If an UImapID is not listed, that zone has no associated factionID
+    -- see https://wow.gamepedia.com/UiMapID/Classic for the Classic list of UImapIDs
+    -- see https://wow.gamepedia.com/FactionID for the list of factionIDs
+
+    local zonesAndFactions = {
+        --------- Horde ----------
+        [1411]      = 46,       -- Durotar/Orgrimmar
+        [1454]      = 46,       -- Orgrimmar/Orgrimmar
+        [1412]      = 81,       -- Mulgore/Thunder Bluff
+        [1456]      = 81,       -- Thunder Bluff/Thunder Bluff
+        [1421]      = 68,       -- Silverpine Forest/Undercity
+        [1458]      = 68,       -- Undercity/Undercity
+        [1420]      = 68,       -- Tirisfal Glades/Undercity
+
+        --------- Alliance ----------
+        [1453]      = 72,       -- Stormwind City/Stormwind
+        [1429]      = 72,       -- Elwynn Forest/Stormwind
+        [1436]      = 72,       -- Westfall/Stormwind
+        [1433]      = 72,       -- Redridge Mountains/Stormwind
+        [1437]      = 75,       -- Wetlands/Stormwind
+        [1419]      = 72,       -- Blasted Lands/Stormwind
+        [1426]      = 47,       -- Dun Morogh/Ironforge
+        [1432]      = 47,       -- Loch Modan/Ironforge
+        [1455]      = 47,       -- Ironforge/Ironforge
+        [1427]      = 47,       -- Searing Gorge/Ironforge
+        [1457]      = 69,       -- Darnassus/Darnassus
+        [1438]      = 69,       -- Teldrassil/Darnassus
+        [1439]      = 69,       -- Darkshore/Darnassus
+        [1450]      = 69,       -- Moonglade/Darnassus
+
+        --------- Both ---------
+        [1422]      = 529,      -- Western Plaguelands/Argent Dawn
+        [1423]      = 529,      -- Eastern Plaguelands/Argent Dawn
+        [1446]      = 369,      -- Tanaris/Gadgetzan
+        [1451]      = 609,      -- Silithus/Cenarion Circle
+        [1452]      = 577,      -- Winterspring/Everlook
+        [1425]      = A and 1174 or H and 530, -- The Hinterlands/Wildhammer Clan or Darkspear Trolls
+        [1431]      = A and 72 or H and 68, -- Duskwood/Stormwind or Undercity
+        [1440]      = A and 69 or H and 68, -- Ashenvale/Darnassus or Undercity
+        [1444]      = A and 69 or H and 46, -- Feralas/Darnassus or Orgrimmar
+        [1413]      = A and 72 or H and 81, -- The Barrens/Stormwind or Thunder Bluff
+        [1417]      = A and 47 or H and 530, -- Arathi Highlands/Ironforge or Darkspear Trolls
+        [1424]      = A and 72 or H and 68, -- Hillsbrad Foothills/Stormwind/Undercity
+        [1416]      = A and 72 or H and 46, -- Alterac Mountains/Stormwind or Orgrimmar
+        [1418]      = A and 47 or H and 46, -- Badlands/Ironforge or Orgrimmar
+        [1428]      = A and 47 or H and 530, -- Burning Steppes/Ironforge or Darkspear Trolls
+        [1438]      = A and 72 or H and 530, -- Stranglethorn Vale/Stormwind or Darkspear Trolls
+        [1435]      = A and 72 or H and 46, -- Swamp of Sorrows/Stormwind or Orgrimmar
+        [1441]      = A and 69 or H and 81, -- Thousand Needles/Darnassus or Thunder Bluff
+        [1442]      = A and 54 or H and 81, -- Stonetalon Mountains/Gnomeregan Exiles orThunder Bluff
+        [1443]      = A and 47 or H and 81, -- Desolace/Ironforge or Thunder Bluff
+        [1445]      = A and 72 or H and 46, -- Dustwallow Marsh/Stormwind or Orgrimmar
+        [1447]      = A and 47 or H and 46, -- Azshara/Ironforge or Orgrimmar
+        [1448]      = A and 69 or H and 68, -- Felwood/Darnassus or Undercity
+
+    }
+    return zonesAndFactions
+end
+
+function RepByZone:InstancesAndFactionList()
+    local instancesAndFactions = {
+        -- instanceID = factionID
+        -- If an instanceID is not listed, that instance has no associated factionID
+        -- See https://wow.gamepedia.com/InstanceID for the list of instanceIDs
+
+        --------- Dungeons ----------
+        [34]        = 72, -- The Stockades/Stormwind
+        [90]        = 54, -- Gnomeregan/Gnomeregan Exiles
+        [389]       = 46, -- Ragefire Chasm/Orgrimmar
+        [349]       = 609, -- Maraudon/Cenarion Circle
+        [1001]      = 529, -- Scarlet Halls/Argent Dawn
+        [1004]      = 529, -- Scarlet Monastary/Argent Dawn
+        [1007]      = 529, -- Scholomance/Argent Dawn
+        [33]        = 68, -- Shadowfang Keep/Undercity
+        [329]       = 529, -- Strathholme/Argent Dawn
+        [36]        = 72, -- The Deadmines/Stormwind
+        [230]       = 59, -- Blackrock Depths/Thorium Brotherhood
+        [109]       = 910, -- Temple of Atal'Hakkar (Sunken Temple)/Brood of Nozdormu
+        [43]        = 81, -- Wailing Caverns/Thunder Bluff
+        [129]       = A and 72 or H and 46, -- Razorfen Downs/Stormwind or Orgrimmar
+        [47]        = A and 69 or H and 81, -- Razorfen Kraul/Darnassus or Thunderbluff
+        [48]        = A and 69 or H and 530, -- Blackfathom Deeps/Darnassus or Darkspear Trolls
+        [229]       = A and 72 or H and 46, -- Blackrock Spire/Stormwind or Orgrimmar
+        [70]        = A and 47 or H and 530, -- Uldaman/Ironforge or Darkspear Trolls
+        [209]       = A and 1174 or H and 530, -- Zul'Farrak/Wildhammer Clan or Darkspear Trolls
+
+        ----------- Battlegrounds ----------
+        [30]        = A and 730 or H and 729, -- Alterac Valley/Stormpike Guard or Frostwolf Clan
+        [529]       = A and 509 or H and 510, -- Arathi Basin/The Leage of Arathor or The Defilers
+        [589]       = A and 890 or H and 889, -- Warsong Gulch/Silverwing Sentinels or Warsong Outriders
+
+        ---------- Raids -----------
+        [409]       = 749, -- Molten Core/Hydraxian Waterlords
+        [509]       = 910, -- Ruins of Ahn'Qiraj/Brood of Nozdormu
+        [531]       = 910, -- Temple of Ahn'Qiraj/Brood of Nozdormu
+        [469]       = A and 72 or H and 46, -- Blackwing Lair/Stormwind or Orgrimmar
+    }
+    return instancesAndFactions
+end
+
+function RepByZone:SubZonesAndFactions()
+    local subZonesAndFactions = {
+        [BS["Tinker Town"]] = 54, -- Gnomeregan Exiles
+        [BS["Dwarven District"]] = 47, -- Ironforge
+        [BS["Booty Bay"]] = 21, -- Booty Bay
+        [BS["The Salty Sailor Tavern"]] = 21, -- Booty Bay
+        [BS["Faldir's Cove"]] = 21, -- Booty Bay
+        [BS["Deadwood Village"]] = 579, -- Timbermaw Hold
+        [BS["Felpaw Village"]] = 579, -- Timbermaw Hold
+        [BS["Timbermaw Hold"]] = 579, -- Timbermaw Hold
+        [BS["Galen's Fall"]] = 68, -- Undercity
+        [BS["Northfold Manor"]] = 349, -- Ravenholt
+        [BS["Stromgarde Keep"]] = 349, -- Ravenholt
+        [BS["The Drowned Reef"]] = 21, -- Booty Bay
+        [BS["Ravenholdt Manor"]] = 349, -- Ravenholt
+        [BS["Sofera's Naze"]] = A and 72, -- Stormwind
+        [BS["Strahnbrad"]] = 349, -- Ravenholt
+        [BS["Alterac Mountains"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
+        [BS["Alterac Valley"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
+        [BS["Azurelode Mine"]] = 72, -- Stormwind
+        [BS["Corrahn's Dagger"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
+        [BS["Gavin's Naze"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
+        [BS["Purgation Isle"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
+        [BS["The Headland"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
+        [BS["Thorium Point"]] = 59, -- Thorium Brotherhood
+        [BS["Ruins of Taurajo"]] = H and 81, -- Thunder Bluff
+		[BS["Spearhead"]] = H and 81, -- Thunder Bluff
+		[BS["Vendetta Point"]] = H and 81, -- Thunder Bluff
+		[BS["Bael Modan"]] = H and 81 or A and 47, -- Thunder Bluff or Ironforge
+		[BS["Bael Modan Excavation"]] = H and 81 or A and 47, -- Thunder Bluff or Ironforge
+		[BS["Bael'dun Keep"]] = H and 81 or A and 47, -- Thunder Bluff or Ironforge
+		[BS["Frazzlecraz Motherlode"]] = A and 47, -- Ironforge
+        [BS["Twinbraid's Patrol"]] = A and 47, -- Ironforge
+        [BS["Arikara's Needle"]] = H and 81 or A and 69, -- Thunder Bluff or Darnassus
+		[BS["Darkcloud Pinnacle"]] = H and 81 or A and 69, -- Thunder Bluff or Darnassus
+        [BS["Freewind Post"]] = H and 81 or A and 69, -- Thunder Bluff or Darnassus
+        [BS["The Bulwark"]] = 529, -- Argent Dawn
+        [BS["Andorhal"]] = A and 72 or H and 68, -- Stormwind or Undercity
+		[BS["Chillwind Camp"]] = A and 72, -- Stormwind
+		[BS["Sorrow Hill"]] = A and 72, -- Stormwind
+        [BS["Uther's Tomb"]] = A and 72, -- Stormwind
+        [BS["Direforge Hill"]] = A and 69, -- Darnassus
+		[BS["Greenwarden's Grove"]] = A and 69, -- Darnassus
+		[BS["Menethil Harbor"]] = A and 72, -- Stormwind
+        [BS["The Green Belt"]] = A and 69, -- Darnassus
+        [BS["Frostfire Hot Springs"]] = 579, -- Timbermaw Hold
+        [BS["Timbermaw Post"]] = 579, -- Timbermaw Hold
+        [BS["Winterfall Village"]] = 579, -- Timbermaw Hold
+        [BS["Frostsaber Rock"]] = A and 589, -- Wintersaber Trainers
+        [BS["Dun Modr"]] = A and 47, -- Ironforge
+    }
+    return subZonesAndFactions
+end
