69958ccb8102c35bc8ac77efd4879a63844b8ea5
myrroddin <myrroddin@a59a231c-ed30-472f-94b8-dd837922ea07>
2019-10-28 15:23:58 +0000

    - Code optimization and clean up - Update more zones and subzones - First pass through Horde content - Adjust Alliance content

--- trunk/Data.lua
+++ trunk/Data.lua
@@ -44,21 +44,21 @@ function RepByZone:ZoneAndFactionList()
         [1452]      = 577,      -- Winterspring/Everlook
         [1425]      = A and 1174 or H and 530, -- The Hinterlands/Wildhammer Clan or Darkspear Trolls
         [1431]      = A and 72 or H and 68, -- Duskwood/Stormwind or Undercity
-        [1440]      = A and 69 or H and 68, -- Ashenvale/Darnassus or Undercity
-        [1444]      = A and 69 or H and 46, -- Feralas/Darnassus or Orgrimmar
+        [1440]      = A and 69 or H and 46, -- Ashenvale/Darnassus or Orgrimmar
+        [1444]      = A and 69 or H and 81, -- Feralas/Darnassus or Thunder Bluff
         [1413]      = A and 470 or H and 81, -- The Barrens/Ratchet or Thunder Bluff
         [1417]      = A and 72 or H and 530, -- Arathi Highlands/Stormwind or Darkspear Trolls
         [1424]      = A and 72 or H and 68, -- Hillsbrad Foothills/Stormwind/Undercity
         [1416]      = A and 72 or H and 46, -- Alterac Mountains/Stormwind or Orgrimmar
         [1418]      = A and 47 or H and 46, -- Badlands/Ironforge or Orgrimmar
         [1428]      = A and 47 or H and 530, -- Burning Steppes/Ironforge or Darkspear Trolls
-        [1438]      = A and 72 or H and 530, -- Stranglethorn Vale/Stormwind or Darkspear Trolls
+        [1438]      = A and 72 or H and 76, -- Stranglethorn Vale/Stormwind or Orgrimmar
         [1435]      = A and 72 or H and 46, -- Swamp of Sorrows/Stormwind or Orgrimmar
         [1441]      = A and 69 or H and 81, -- Thousand Needles/Darnassus or Thunder Bluff
         [1442]      = A and 69 or H and 81, -- Stonetalon Mountains/Darnassus or Thunder Bluff
-        [1443]      = A and 47 or H and 81, -- Desolace/Ironforge or Thunder Bluff
+        [1443]      = A and 72 or H and 81, -- Desolace/Stormwind or Thunder Bluff
         [1445]      = A and 72 or H and 46, -- Dustwallow Marsh/Stormwind or Orgrimmar
-        [1447]      = A and 47 or H and 46, -- Azshara/Ironforge or Orgrimmar
+        [1447]      = A and 69 or H and 46, -- Azshara/Darnassus or Orgrimmar
         [1448]      = A and 69 or H and 68, -- Felwood/Darnassus or Undercity
 
     }
@@ -83,7 +83,7 @@ function RepByZone:InstancesAndFactionList()
         [329]       = 529, -- Strathholme/Argent Dawn
         [36]        = 72, -- The Deadmines/Stormwind
         [230]       = 59, -- Blackrock Depths/Thorium Brotherhood
-        [109]       = 910, -- Temple of Atal'Hakkar (Sunken Temple)/Brood of Nozdormu
+        [109]       = 270, -- Temple of Atal'Hakkar (Sunken Temple)/Zandalar Tribe
         [43]        = 81, -- Wailing Caverns/Thunder Bluff
         [429]       = 809, -- Dire Maul/Shen'dralar
         [369]       = 72, -- Deeprun Tram/Stormwind
@@ -104,9 +104,9 @@ function RepByZone:InstancesAndFactionList()
         [509]       = 609, -- Ruins of Ahn'Qiraj/Cenarion Circle
         [531]       = 910, -- Temple of Ahn'Qiraj/Brood of Nozdormu
         [533]       = 529, -- Naxxramas/Argent Dawn
+        [309]       = 270, -- Zul'Gurub/Zandalar Tribe
         [469]       = A and 72 or H and 46, -- Blackwing Lair/Stormwind or Orgrimmar
         [249]       = A and 72 or H and 46, -- Onyxia's Lair/Stormwind or Orgrimmar
-        [309]       = A and 72 or H and 530, -- Zul'Gurub/Stormwind or Darkspear Trolls
     }
     return instancesAndFactions
 end
@@ -126,7 +126,7 @@ function RepByZone:SubZonesAndFactions()
         [BS["Stromgarde Keep"]] = 349, -- Ravenholt
         [BS["The Drowned Reef"]] = 21, -- Booty Bay
         [BS["Ravenholdt Manor"]] = 349, -- Ravenholt
-        [BS["Sofera's Naze"]] = A and 72, -- Stormwind
+        [BS["Sofera's Naze"]] = 72, -- Stormwind
         [BS["Strahnbrad"]] = 349, -- Ravenholt
         [BS["Alterac Mountains"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
         [BS["Alterac Valley"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
@@ -136,25 +136,25 @@ function RepByZone:SubZonesAndFactions()
         [BS["Purgation Isle"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
         [BS["The Headland"]] = A and 730 or H and 729, -- Stormpike Guard or Frostwolf Clan
         [BS["Thorium Point"]] = 59, -- Thorium Brotherhood
-        [BS["Ruins of Taurajo"]] = H and 81, -- Thunder Bluff
-		[BS["Spearhead"]] = H and 81, -- Thunder Bluff
-		[BS["Vendetta Point"]] = H and 81, -- Thunder Bluff
+        [BS["Ruins of Taurajo"]] = 81, -- Thunder Bluff
+		[BS["Spearhead"]] = 81, -- Thunder Bluff
+		[BS["Vendetta Point"]] = 81, -- Thunder Bluff
 		[BS["Bael Modan"]] = H and 81 or A and 47, -- Thunder Bluff or Ironforge
 		[BS["Bael Modan Excavation"]] = H and 81 or A and 47, -- Thunder Bluff or Ironforge
 		[BS["Bael'dun Keep"]] = H and 81 or A and 47, -- Thunder Bluff or Ironforge
-		[BS["Frazzlecraz Motherlode"]] = A and 47, -- Ironforge
-        [BS["Twinbraid's Patrol"]] = A and 47, -- Ironforge
+		[BS["Frazzlecraz Motherlode"]] = 47, -- Ironforge
+        [BS["Twinbraid's Patrol"]] = 47, -- Ironforge
         [BS["Arikara's Needle"]] = H and 81 or A and 69, -- Thunder Bluff or Darnassus
 		[BS["Darkcloud Pinnacle"]] = H and 81 or A and 69, -- Thunder Bluff or Darnassus
         [BS["Freewind Post"]] = H and 81 or A and 69, -- Thunder Bluff or Darnassus
         [BS["The Bulwark"]] = 529, -- Argent Dawn
         [BS["Andorhal"]] = A and 72 or H and 68, -- Stormwind or Undercity
-		[BS["Chillwind Camp"]] = A and 72, -- Stormwind
-		[BS["Sorrow Hill"]] = A and 72, -- Stormwind
-        [BS["Uther's Tomb"]] = A and 72, -- Stormwind
-        [BS["Direforge Hill"]] = A and 69, -- Darnassus
-		[BS["Greenwarden's Grove"]] = A and 69, -- Darnassus
-        [BS["The Green Belt"]] = A and 69, -- Darnassus
+		[BS["Chillwind Camp"]] = 72, -- Stormwind
+		[BS["Sorrow Hill"]] = 72, -- Stormwind
+        [BS["Uther's Tomb"]] = 72, -- Stormwind
+        [BS["Direforge Hill"]] = 69, -- Darnassus
+		[BS["Greenwarden's Grove"]] = 69, -- Darnassus
+        [BS["The Green Belt"]] = 69, -- Darnassus
         [BS["Frostfire Hot Springs"]] = 579, -- Timbermaw Hold
         [BS["Timbermaw Post"]] = 579, -- Timbermaw Hold
         [BS["Winterfall Village"]] = 579, -- Timbermaw Hold
@@ -162,10 +162,31 @@ function RepByZone:SubZonesAndFactions()
         [BS["Gelkis Village"]] = 92, -- Gelkis Clan Centaur
         [BS["Magram Territory"]] = 93, -- Magram Clan Centaur
         [BS["Ratchet"]] = 470, -- Ratchet
-        [BS["Deeprun Tram"]] = A and 72, -- Stormwind
-        [BS["Rut'theran Village"]] = A and 69, -- Darnassus
+        [BS["Deeprun Tram"]] = 72, -- Stormwind
+        [BS["Rut'theran Village"]] = 69, -- Darnassus
         [BS["Gnomeregan"]] = 54, -- Gnomeregan Exiles
         [BS["Ethel Rethor"]] = 529, -- Argent Dawn
+        [BS["Yojamba Isle"]] = 270, -- Zandalar Tribe
+        [BS["Kormek's Hut"]] = 470, -- Ratchet
+        [BS["Scrabblescrew's Camp"]] = 470, -- Ratchet
+        [BS["Shadowprey Village"]] = 530, -- Darkspear Trolls
+        [BS["Ranazjar Isle"]] = 69, -- Darnassus
+        [BS["Valley of Wisdom"]] = 81, -- Thunder Bluff
+        [BS["Valley of Spirits"]] = 530, -- Darkspear Trolls
+        [BS["Zoram'gar Outpost"]] = 530, -- Darkspear Trolls
+        [BS["Quel'Danil Lodge"]] = 69, -- Darnassus
+        [BS["The Shimmering Flats"]] = 54, -- Gnomeregan Exiles
+        [BS["Mirage Raceway"]] = 54, -- Gnomeregan Exiles
+        [BS["Darkspear Strand"]] = 530, -- Darkspear Trolls
+        [BS["Sen'jin Village"]] = 530, -- Darkspear Trolls
+        [BS["Echo Isles"]] = 530, -- Darkspear Trolls
+        [BS["Nesingwary's Expedition"]] = 47, -- Ironforge
+        [BS["Kal'ai Ruins"]] = 270, -- Zandalar Tribe
+        [BS["Zuuldaia Ruins"]] = 270, -- Zandalar Tribe
+        [BS["Ziata'jai Ruins"]] = 270, -- Zandalar Tribe
+        [BS["Ruins of Jubuwal"]] = 270, -- Zandalar Tribe
+        [BS["Ruins of Aboraz"]] = 270, -- Zandalar Tribe
+        [BS["Gurubashi Arena"]] = 87, -- Bloodsail Buccaneers
     }
     return subZonesAndFactions
 end
