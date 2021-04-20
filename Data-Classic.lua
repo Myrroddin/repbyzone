424a23ca554e5ff5d279fff33c1310bcce73895a
Paul Vandersypen <psvander@gmail.com>
2021-04-19 20:40:29 -0600

    [Classic & TBC] Organize instanceIDs for my sanity

--- Data-Classic.lua
+++ Data-Classic.lua
@@ -71,27 +71,26 @@ function RepByZone:InstancesAndFactionList()
         -- See https://wow.gamepedia.com/InstanceID for the list of instanceIDs
 
         --------- Dungeons ----------
+        [33]        = 68, -- Shadowfang Keep/Undercity
         [34]        = 72, -- The Stockades/Stormwind
+        [36]        = 72, -- The Deadmines/Stormwind
+        [43]        = 81, -- Wailing Caverns/Thunder Bluff
+        [47]        = A and 69 or H and 81, -- Razorfen Kraul/Darnassus or Thunderbluff
+        [48]        = A and 69 or H and 530, -- Blackfathom Deeps/Darnassus or Darkspear Trolls
+        [70]        = A and 47 or H and 530, -- Uldaman/Ironforge or Darkspear Trolls
         [90]        = 54, -- Gnomeregan/Gnomeregan Exiles
-        [389]       = 76, -- Ragefire Chasm/Orgrimmar
+        [109]       = 270, -- Temple of Atal'Hakkar (Sunken Temple)/Zandalar Tribe
+        [129]       = A and 72 or H and 76, -- Razorfen Downs/Stormwind or Orgrimmar
+        [209]       = A and 471 or H and 530, -- Zul'Farrak/Wildhammer Clan or Darkspear Trolls
+        [229]       = A and 72 or H and 76, -- Blackrock Spire/Stormwind or Orgrimmar
+        [230]       = 59, -- Blackrock Depths/Thorium Brotherhood
+        [329]       = 529, -- Strathholme/Argent Dawn
         [349]       = 609, -- Maraudon/Cenarion Circle
+        [389]       = 76, -- Ragefire Chasm/Orgrimmar
+        [429]       = 809, -- Dire Maul/Shen'dralar
         [1001]      = 529, -- Scarlet Halls/Argent Dawn
         [1004]      = 529, -- Scarlet Monastary/Argent Dawn
         [1007]      = 529, -- Scholomance/Argent Dawn
-        [33]        = 68, -- Shadowfang Keep/Undercity
-        [329]       = 529, -- Strathholme/Argent Dawn
-        [36]        = 72, -- The Deadmines/Stormwind
-        [230]       = 59, -- Blackrock Depths/Thorium Brotherhood
-        [109]       = 270, -- Temple of Atal'Hakkar (Sunken Temple)/Zandalar Tribe
-        [43]        = 81, -- Wailing Caverns/Thunder Bluff
-        [429]       = 809, -- Dire Maul/Shen'dralar
-        [369]       = 72, -- Deeprun Tram/Stormwind
-        [129]       = A and 72 or H and 76, -- Razorfen Downs/Stormwind or Orgrimmar
-        [47]        = A and 69 or H and 81, -- Razorfen Kraul/Darnassus or Thunderbluff
-        [48]        = A and 69 or H and 530, -- Blackfathom Deeps/Darnassus or Darkspear Trolls
-        [229]       = A and 72 or H and 76, -- Blackrock Spire/Stormwind or Orgrimmar
-        [70]        = A and 47 or H and 530, -- Uldaman/Ironforge or Darkspear Trolls
-        [209]       = A and 471 or H and 530, -- Zul'Farrak/Wildhammer Clan or Darkspear Trolls
 
         ----------- Battlegrounds ----------
         [30]        = A and 730 or H and 729, -- Alterac Valley/Stormpike Guard or Frostwolf Clan
@@ -99,13 +98,13 @@ function RepByZone:InstancesAndFactionList()
         [589]       = A and 890 or H and 889, -- Warsong Gulch/Silverwing Sentinels or Warsong Outriders
 
         ---------- Raids -----------
+        [249]       = A and 72 or H and 76, -- Onyxia's Lair/Stormwind or Orgrimmar
+        [309]       = 270, -- Zul'Gurub/Zandalar Tribe
         [409]       = 749, -- Molten Core/Hydraxian Waterlords
+        [469]       = A and 72 or H and 76, -- Blackwing Lair/Stormwind or Orgrimmar
         [509]       = 609, -- Ruins of Ahn'Qiraj/Cenarion Circle
         [531]       = 910, -- Temple of Ahn'Qiraj/Brood of Nozdormu
         [533]       = 529, -- Naxxramas/Argent Dawn
-        [309]       = 270, -- Zul'Gurub/Zandalar Tribe
-        [469]       = A and 72 or H and 76, -- Blackwing Lair/Stormwind or Orgrimmar
-        [249]       = A and 72 or H and 76, -- Onyxia's Lair/Stormwind or Orgrimmar
     }
     return instancesAndFactions
 end
